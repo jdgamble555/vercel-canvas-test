35f211a11f26a2c2f257110645521bb6a3b96612
Jonathan Gamble <jdgamble555@gmail.com>
2024-01-01 13:50:37 -0600

    first commit

---
+++ src/routes/test/+server.ts
@@ -0,0 +1,33 @@
+import { type RequestHandler } from "@sveltejs/kit";
+import { createCanvas } from 'canvas';
+
+export const GET = (async () => {
+
+    const canvas = createCanvas(1200, 800);
+    const ctx = canvas.getContext('2d');
+
+    // Set the style for the title
+    ctx.font = "72px Arial";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "black";
+    ctx.fillText("Code.Build" + (Math.random() + 1).toString(36).substring(7), canvas.width / 2, canvas.height / 2 - 20);
+
+    ctx.fillStyle = '#0369a1';
+    ctx.fillRect(0, 0, canvas.width, 40);
+
+
+    // Draw a black border around the canvas
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.strokeRect(1, 1, canvas.width - 2, canvas.height - 2);
+
+    const buffer = canvas.toBuffer('image/png');
+
+    return new Response(buffer, {
+        headers: {
+            'Content-Type': 'image/png',
+            'Content-Length': buffer.length.toString()
+        }
+    });
+
+}) satisfies RequestHandler;
